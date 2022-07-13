24881a0e5665a3ba5b7551ac3ae21b25bd105322
Dylan Mitchley <[dylanmitchley1994@gmail.com]>
2022-07-13 19:18:38 +0200

    third commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 // importing the routes
 const routes = require("./routes/index");
 const helmet = require("helmet");
@@ -19,17 +19,17 @@ app.use(morgan("dev"));
 // --------------------------deployment------------------------------
 // const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
 
 // accessing the routes from the routes folder with /api
